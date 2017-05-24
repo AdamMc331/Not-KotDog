294167ee20ee4b1d1659bf6a101f30dc465db71d
AdamMc331 <amcneilly331@gmail.com>
2017-05-24 14:32:32 -0400

    Added camera and gallery intents to select images.

--- app/src/main/java/com/clarifai/notkotdog/MainActivity.kt
+++ app/src/main/java/com/clarifai/notkotdog/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.clarifai.notkotdog
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.FileProvider
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
@@ -19,6 +27,8 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import timber.log.Timber
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     var manager: ClarifaiManager? = null
@@ -35,9 +45,13 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            AlertDialog.Builder(this)
+                    .setMessage("Select An Image")
+                    .setPositiveButton("Gallery") { _, _ -> startGalleryChooser() }
+                    .setNegativeButton("Camera") { _, _ -> startCameraChooser() }
+                    .create()
+                    .show()
         }
 
         authorizeUser()
@@ -93,8 +107,65 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            //TODO:
+            //uploadImage(data.getData());
+        } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            val photoUri: Uri = FileProvider.getUriForFile(this, applicationContext.packageName + ".provider", getCameraFile());
+            //TODO:
+            //uploadImage(photoUri);
+        }
+    }
+
+    private fun startGalleryChooser() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
+                    GALLERY_IMAGE_REQUEST)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), GALLERY_PERMISSIONS_REQUEST)
+        }
+    }
+
+    private fun startCameraChooser() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val photoUri = FileProvider.getUriForFile(this, applicationContext.packageName + ".provider", getCameraFile())
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            startActivityForResult(intent, CAMERA_IMAGE_REQUEST)
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSIONS_REQUEST)
+        }
+    }
+
+    private fun getCameraFile(): File {
+        val dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File(dir, FILE_NAME)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            GALLERY_PERMISSIONS_REQUEST -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) startGalleryChooser()
+            CAMERA_PERMISSIONS_REQUEST -> if (grantResults[0] == PackageManager.PERMISSION_GRANTED) startCameraChooser()
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
     companion object {
         private val MEDIA_TYPE_JSON = MediaType.parse("application/json; charset=utf8")
         private val GRANT_TYPE_CREDENTIALS = "\"grant_type\":\"client_credentials\""
+
+        private val GALLERY_PERMISSIONS_REQUEST = 0
+        private val GALLERY_IMAGE_REQUEST = 1
+        private val CAMERA_PERMISSIONS_REQUEST = 2
+        private val CAMERA_IMAGE_REQUEST = 3
+
+        private val FILE_NAME = "temp.jpg"
     }
 }
